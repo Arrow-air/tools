28fddc8d102c597ae3573d91d820f343c659ae7b
A.M. Smith <smitham42@gmail.com>
2024-09-11 06:28:52 -0700

    feat: update references

--- local-dev/docker-compose/cockroachdb/scripts/init.sql
+++ local-dev/docker-compose/cockroachdb/scripts/init.sql
@@ -1,4 +1,4 @@
 /* Initializes empty arrow database with svc_storage user, only on first database run */
-CREATE DATABASE arrow;
+CREATE DATABASE realm;
 CREATE USER svc_storage;
-GRANT ALL PRIVILEGES ON DATABASE arrow TO svc_storage;
+GRANT ALL PRIVILEGES ON DATABASE realm TO svc_storage;
